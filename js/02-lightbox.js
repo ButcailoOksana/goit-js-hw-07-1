905403e7856d4005efdf7303d11bd93f47bd1b67
ButcailoOksana <oksanabutcailo@gmail.com>
2022-08-13 15:17:48 +0300

    02 lightbox зроблено

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = createMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", openModal);
+// console.log(galleryEl);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+ 		<a class ="gallery__link" href ="${original}">
+		<img class ="gallery__image"
+ 		src ="${preview}"
+ 		alt =${description};
+ 		/>
+		</a>`;
+    })
+    .join("");
+}
+
+function openModal(evt) {
+  evt.preventDefault();
+  if (evt.target.nodName !== "IMG") return;
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+lightbox.next();
