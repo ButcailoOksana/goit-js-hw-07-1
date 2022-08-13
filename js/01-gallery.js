364373a968cd3d3b3dc8e41155c2b8d34a50209a
ButcailoOksana <oksanabutcailo@gmail.com>
2022-08-13 11:25:06 +0300

    Завдання 01 зроблено

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+document.addEventListener("keydown", onKeyDown);
+//const lightbox = basicLigthbox.create("");
+
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl);
+
+const markup = createMarkup(galleryItems);
+//console.log(markup);
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+galleryEl.addEventListener("click", onOpenImg);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class ="gallery__item">
+ 		<a class ="gallery__link" href ="${original}">
+		<img class ="gallery__image"
+ 		src ="${preview}"
+ 		data-source ="${original}"
+ 		alt =${description};
+ 		/>
+		</a>
+ 		</div>`;
+    })
+    .join("");
+}
+
+function onOpenImg(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const url = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+   <img src="${url}" width="800" height="600">
+`);
+  instance.show();
+}
+function onKeyDown(evt) {
+  const ESC_KEY_CODE = "Escape";
+  if (evt.code === ESC_KEY_CODE) {
+    lightbox.close();
+  }
+}
